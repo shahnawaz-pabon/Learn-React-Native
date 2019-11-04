dfd8bcf43b8e7cf6b4738bfbf4276a293c6746ba
pabon <s.pabon93@gmail.com>
2019-11-04 14:37:49 +0600

    Some Layout with Flexbox added

--- rn-complete-guide/App.js
+++ rn-complete-guide/App.js
@@ -4,16 +4,54 @@ import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 export default function App() {
   return (
     <View style={{ padding: 30 }}>
-      <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' }}>
+
+      <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', paddingBottom: 30}}>
         <TextInput
           placeholder="Course Goal"
           style={{ width: '80%', borderColor: 'black', borderWidth: 1, padding: 10 }}
         />
         <Button title="ADD"/>
       </View>
-      <View>
+
+      <View
+        style={{
+          backgroundColor: 'red',
+          width: 100,
+          height: 100,
+          justifyContent: 'center',
+          alignItems: 'center'
+        }}
+      >
+        <Text>1</Text>
 
       </View>
+
+      <View
+        style={{
+          backgroundColor: 'blue',
+          width: 100,
+          height: 100,
+          justifyContent: 'center',
+          alignItems: 'center'
+        }}
+      >
+        <Text>2</Text>
+
+      </View>
+
+      <View
+        style={{
+          backgroundColor: 'green',
+          width: 100,
+          height: 100,
+          justifyContent: 'center',
+          alignItems: 'center'
+        }}
+      >
+        <Text>3</Text>
+
+      </View>
+
     </View>
   );
 }
