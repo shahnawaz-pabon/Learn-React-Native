261724efcf27ee533db3eab03f774e3503e69b82
pabon <s.pabon93@gmail.com>
2019-11-04 15:36:40 +0600

    Created custom styles rather than inline styles

--- rn-complete-guide/App.js
+++ rn-complete-guide/App.js
@@ -4,63 +4,34 @@ import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 export default function App() {
   return (
 
-    <View
-      style={{
-        padding: 30,
-        flexDirection: 'row',
-        height: 300,
-        width: '80%',
-        justifyContent: 'space-around',
-        alignItems: 'stretch'
-      }}
-    >
-
-      <View
-        style={{
-          backgroundColor: 'red',
-          // width: 100,
-          // height: 100,
-          flex: 3,
-          justifyContent: 'center',
-          alignItems: 'center'
-        }}
-      >
-        <Text>1</Text>
+    <View style={styles.screen}>
+      <View style={styles.inputContainer}>
+        <TextInput
+          placeholder="Course Goal"
+          style={styles.input}
+        />
+        <Button title="ADD"/>
+      </View>
+      <View>
 
       </View>
-
-      <View
-        style={{
-          backgroundColor: 'blue',
-          // width: 100,
-          // height: 100,
-          flex: 2,
-          justifyContent: 'center',
-          alignItems: 'center'
-        }}
-      >
-        <Text>2</Text>
-
-      </View>
-
-      <View
-        style={{
-          backgroundColor: 'green',
-          // width: 100,
-          // height: 100,
-          flex: 5,
-          justifyContent: 'center',
-          alignItems: 'center'
-        }}
-      >
-        <Text>3</Text>
-
-      </View>
-
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-
+  screen: {
+    padding: 50
+  },
+  inputContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center'
+  },
+  input: {
+    width: '80%',
+    borderColor: 'black',
+    borderWidth: 1,
+    padding: 10
+  }
 });
