fbe36e3b7cd4030de4eb694ed3b8826c8489e391
pabon <s.pabon93@gmail.com>
2019-11-04 17:17:14 +0600

    This is also scrollable but more efficient rather than using ScrollView component

--- rn-complete-guide/App.js
+++ rn-complete-guide/App.js
@@ -1,5 +1,13 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, TextInput, Button, ScrollView } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Button,
+  ScrollView,
+  FlatList
+} from 'react-native';
 
 export default function App() {
 
@@ -11,7 +19,10 @@ export default function App() {
   };
 
   const addGoalHandler = () => {
-    setCourseGoals(currentGoals => [...currentGoals, enteredGoal]);
+    setCourseGoals(currentGoals => [
+      ...currentGoals,
+      { key: Math.random().toString(), value: enteredGoal }
+    ]);
     // console.log(enteredGoal);
   };
 
@@ -27,15 +38,14 @@ export default function App() {
         <Button title="ADD" onPress={addGoalHandler}/>
       </View>
 
-      <ScrollView>
-        <View>
-          {courseGoals.map(goal => (
-            <View key={goal} style={styles.listItem}>
-              <Text>{goal}</Text>
-            </View>
-          ))}
-        </View>
-      </ScrollView>
+      <FlatList
+        data={courseGoals}
+        renderItem={itemData => (
+          <View style={styles.listItem}>
+            <Text>{itemData.item.value}</Text>
+          </View>
+        )}
+      />
 
     </View>
   );
