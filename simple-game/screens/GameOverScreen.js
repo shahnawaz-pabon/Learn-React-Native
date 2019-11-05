6dca8ce6b3ce6cbc194d9fd75453a94a245b550c
pabon <s.pabon93@gmail.com>
2019-11-05 17:18:38 +0600

    GameOverScreen added and game over action handled successfully

---
+++ simple-game/screens/GameOverScreen.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import Colors from '../constants/colors';
+
+const GameOverScreen = props => {
+  return (
+    <View style={styles.screen}>
+      <Text>The Game is Over!</Text>
+      <Text>Number of rounds: {props.roundsNumber}</Text>
+      <Text>Number was: {props.userNumber}</Text>
+      <Button title="New Game" onPress={props.onRestart}/>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+});
+
+export default GameOverScreen;
