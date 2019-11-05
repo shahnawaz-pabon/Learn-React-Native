d4b9a3255a8e006ce4ec62ce6e885da5427e6433
pabon <s.pabon93@gmail.com>
2019-11-05 15:51:07 +0600

    Custom GameScreen for opponent's guess added and styled

--- simple-game/constants/colors.js
+++ simple-game/constants/colors.js
@@ -2,5 +2,6 @@ export default {
   primary: '#f7287b',
   accent: '#c717fc',
   dark_blue: '#2c3e50',
-  secondary: '#27ae60'
+  secondary: '#27ae60',
+  light_yellow: '#ffcb65'
 }
---
+++ simple-game/screens/GameScreen.js
@@ -0,0 +1,50 @@
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import Card from '../components/Card';
+import NumberContainer from '../components/NumberContainer';
+
+const generateRandomBetween = (min, max, exclude) => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  const rndNum = Math.floor(Math.random() * (max - min)) + min;
+  if(rndNum === exclude){
+    return generateRandomBetween(min, max, exclude);
+  } else {
+    return rndNum;
+  }
+}
+
+const GameScreen = props => {
+
+  const [currentGuess, setCurrentGuess] = useState(
+    generateRandomBetween(1, 100, props.userChoice)
+  );
+
+  return (
+    <View style={styles.screen}>
+      <Text>Opponent's Guess</Text>
+      <NumberContainer>{currentGuess}</NumberContainer>
+      <Card style={styles.buttonContainer}>
+        <Button title="Lower" onPress={() => {}}/>
+        <Button title="Greater" onPress={() => {}}/>
+      </Card>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    padding: 10,
+    alignItems: 'center'
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    marginTop: 20,
+    width: 300,
+    maxWidth: '80%'
+  }
+});
+
+export default GameScreen;
