b8239ad622e1caf5ad37f4a9e83f3d28a9fe1fa8
pabon <s.pabon93@gmail.com>
2019-11-06 19:46:03 +0600

    Navigation package added and screens created

---
+++ TravelArticleApp/src/screens/Article.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+  View,
+  Text
+} from 'react-native';
+
+export default class Article extends Component{
+  render(){
+    return(
+      <View>
+        <Text>Article!!!</Text>
+      </View>
+    );
+  }
+}
+
+
+const styles = StyleSheet.create({
+});
---
+++ TravelArticleApp/src/screens/List.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+  View,
+  Text
+} from 'react-native';
+
+export default class List extends Component{
+  render(){
+    return(
+      <View>
+        <Text>List!!!</Text>
+      </View>
+    );
+  }
+}
+
+
+const styles = StyleSheet.create({
+});
