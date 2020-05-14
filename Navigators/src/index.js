9dc87e143416045d2a67eb72339fa42bb73b3dcb
Shahnawaz Hossan <s.pabon93@gmail.com>
2020-05-14 23:24:42 +0600

    Drawer Navigator initially working

--- Navigators/src/index.js
+++ Navigators/src/index.js
@@ -14,32 +14,95 @@ import {
     View,
     Text,
     StatusBar,
+    Button
 } from 'react-native';
 
-export default class App extends React.Component {
+import { createAppContainer } from 'react-navigation';
+
+import { createDrawerNavigator } from 'react-navigation-drawer';
+
+class MyHomeScreen extends React.Component {
+    static navigationOptions = {
+        drawerLabel: 'Home',
+        //   drawerIcon: ({ tintColor }) => (
+        //     <Image
+        //       source={require('./chats-icon.png')}
+        //       style={[styles.icon, { tintColor: tintColor }]}
+        //     />
+        //   ),
+    };
 
     render() {
         return (
-            <>
-                <StatusBar barStyle="dark-content" />
-                <SafeAreaView>
-                    <ScrollView
-                        contentInsetAdjustmentBehavior="automatic"
-                        style={styles.scrollView}>
-                        <View style={styles.body}>
-                            <Text>Hello Navigators</Text>
-                        </View>
-                    </ScrollView>
-                </SafeAreaView>
-            </>
+            <Button
+                onPress={() => 
+                    // this.props.navigation.navigate('Notifications')
+                    this.props.navigation.toggleDrawer()
+                }
+                title="Go to notifications"
+            />
         );
     }
-};
+}
+
+class MyNotificationsScreen extends React.Component {
+    static navigationOptions = {
+        drawerLabel: 'Notifications',
+        //   drawerIcon: ({ tintColor }) => (
+        //     <Image
+        //       source={require('./notif-icon.png')}
+        //       style={[styles.icon, { tintColor: tintColor }]}
+        //     />
+        //   ),
+    };
+
+    render() {
+        return (
+            <Button
+                onPress={() => 
+                    // this.props.navigation.goBack()
+                    this.props.navigation.toggleDrawer()
+                }
+                title="Go back home"
+            />
+        );
+    }
+}
+
+const MyDrawerNavigator = createDrawerNavigator({
+    Home: {
+        screen: MyHomeScreen,
+    },
+    Notifications: {
+        screen: MyNotificationsScreen,
+    },
+});
+
+export default createAppContainer(MyDrawerNavigator);
+
+// export default class App extends React.Component {
+
+//     render() {
+//         return (
+//             <>
+//                 <StatusBar barStyle="dark-content" />
+//                 <SafeAreaView>
+//                     <ScrollView
+//                         contentInsetAdjustmentBehavior="automatic">
+//                         <View style={styles.body}>
+//                             <Text>Hello Navigators</Text>
+//                             {/* <MyApp /> */}
+//                         </View>
+//                     </ScrollView>
+//                 </SafeAreaView>
+//             </>
+//         );
+//     }
+// };
 
 const styles = StyleSheet.create({
-    body: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center'
+    icon: {
+        width: 24,
+        height: 24,
     },
 });
