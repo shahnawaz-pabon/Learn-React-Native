dd2c291a5b7fd5bcfb3e4089402e8c2ed169c811
Shahnawaz Hossan <s.pabon93@gmail.com>
2020-07-13 23:46:53 +0600

    Modalize's handleOpen function corrected

--- modalize/src/index.js
+++ modalize/src/index.js
@@ -11,7 +11,7 @@ export const App = () => {
 
   const handleOpen = () => {
     if (modalizeRef.current) {
-      modalizeRef.current.open();
+      modalizeRef.current.open('top'); // 'default' || 'top'
     }
   };
 
